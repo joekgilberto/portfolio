8b95185e814b9b8bc174c49520527aa71b5dba56
joegilberto <joekgilberto@gmail.com>
2023-08-28 14:32:11 -0400

    archives older materials

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const arrowBtnEl = document.querySelector('.arrow')
-console.log(arrowBtnEl)
-const introEl = document.querySelector('.intro')
-
-arrowBtnEl.addEventListener('click', ()=>{
-    introEl.scrollIntoView({ block: 'end',  behavior: 'smooth' })
-})
