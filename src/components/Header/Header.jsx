c93fe3f6dd6586881e1b2ac3a05f69d73cb856c9
joegilberto <joekgilberto@gmail.com>
2023-09-20 14:52:22 -0400

    moves projects before about in heaader

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,11 +14,11 @@ function Header({page}) {
         <Link to='/'>
           <div className={page==="home"?"active":null} id="first">Home</div>
         </Link>
-        <Link to='/about'>
-          <div className={page==="about"?"active":null}>About</div>
-        </Link>
         <Link to='/projects'>
           <div className={page==="projects"?"active":null}>Projects</div>
+        </Link>
+        <Link to='/about'>
+          <div className={page==="about"?"active":null}>About</div>
         </Link>
         <Link to='/contact'>
           <div className={page==="contact"?"active":null} id="last">Contact</div>
