622ca0eee8177f0a5911a88f1cd58d2af248b349
joekgilberto <joekgilberto@gmail.com>
2023-12-09 14:24:56 -0500

    fixes footer link span

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 function Footer() {
     return (
         <footer>
-            <p>Made by <a href="https://github.com/joekgilberto" target="_blank">Joe Gilberto using</a> <a href="https://react.dev/" target="_blank">React</a>.</p>
+            <p>Made by <a href="https://github.com/joekgilberto" target="_blank">Joe Gilberto</a> using <a href="https://react.dev/" target="_blank">React</a>.</p>
         </footer>
     );
 }
