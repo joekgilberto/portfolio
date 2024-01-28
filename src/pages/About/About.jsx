c2f1881d9da05a1dcc15b12e156fb977b3d2e859
joekgilberto <joekgilberto@gmail.com>
2024-01-27 21:35:33 -0500

    adds skills and skill animations

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -23,33 +23,46 @@ function About({ setPage }) {
     function loaded() {
         return (
             <section className="About">
-
-                <div className="info">
-                    <h1 className="pink-text">About</h1>
-                    <div className="bio">
-                    <div className="quick-facts">
-                        <div className="fact-one">
-                            <img src={require(`../../assets/${about.resumeIcon}`)} alt='resume' />
-                            <p><a className="blue-text" href={about.resume} target="_blank">Resume</a></p>
-                        </div>
-                        <div className="fact-two">
-                            <img src={require(`../../assets/${about.locationIcon}`)} alt='resume' />
-                            <p className="green-text">{about.location}</p>
-                        </div>
-                        <div className="fact-three">
-                            <img src={require(`../../assets/${about.codingIcon}`)} alt='resume' />
-                            <p className="green-text">{about.coding}</p>
+                <h1 className="pink-text">About</h1>
+                <div className="content">
+                    <div className="info">
+                        <div className="bio">
+                            <div className="quick-facts">
+                                <div className="fact-one">
+                                    <img src={require(`../../assets/${about.resumeIcon}`)} alt='resume' />
+                                    <p><a className="blue-text" href={about.resume} target="_blank">Resume</a></p>
+                                </div>
+                                <div className="fact-two">
+                                    <img src={require(`../../assets/${about.locationIcon}`)} alt='resume' />
+                                    <p className="green-text">{about.location}</p>
+                                </div>
+                                <div className="fact-three">
+                                    <img src={require(`../../assets/${about.codingIcon}`)} alt='resume' />
+                                    <p className="green-text">{about.coding}</p>
+                                </div>
+                            </div>
+                            <div className="skills">
+                                {about.skills?.map((skill,idx)=>{
+                                    return <p key={idx} style={{
+                                        border: `2px solid ${idx%3===1?'#ff68b8ff'
+                                        :idx%3===2?'#4a86e8ff'
+                                        :'#5acc29ff'}`,
+                                        animation: `appear .5s forwards ${idx%3===1?'1.75s'
+                                        :idx%3===2?'2s'
+                                        :'2.25s'}`
+                                    }} >{skill}</p>
+                                })}
+                            </div>
+                            <p className="bio-blob">{about.bio1}</p>
+                            <p className="bio-blob">{about.bio2}</p>
+                            <p className="bio-blob">{about.bio3}</p>
                         </div>
                     </div>
-                        <p className="bio-blob">{about.bio1}</p>
-                        <p className="bio-blob">{about.bio2}</p>
-                        <p className="bio-blob">{about.bio3}</p>
-                    </div>
-                </div>
-                <div className="profile-card">
-                    <img className="headshot" src={require(`../../assets/${about.headshot}`)} alt={`${about.name} headshot`} />
-                    <Flare side={'left'} />
+                    <div className="profile-card">
+                        <img className="headshot" src={require(`../../assets/${about.headshot}`)} alt={`${about.name} headshot`} />
+                        <Flare side={'left'} />
 
+                    </div>
                 </div>
             </section>
         )
